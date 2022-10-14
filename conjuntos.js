c7b370cf19e72f9f89f223584477ae7f31cedec3
Angelsalazar23 <angelitosalazar230597@gmail.com>
2022-10-14 14:57:34 -0500

    terminado

---
+++ conjuntos.js
@@ -0,0 +1,15 @@
+
+const familia=['Angel Salazar','Ines Salazar', 'Pablo Salazar', 'Edson Salazar', 'Axel Salazar ', 'America Mendoza']
+
+const familiaSet= new Set(familia)
+
+console.log(familia)
+console.log(familiaSet)
+
+
+familiaSet.add("Angel Salazar")
+console.log(familiaSet)
+
+
+familiaSet.add("javascript")
+console.log(familiaSet)
